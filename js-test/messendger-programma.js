88f464770bd4331fd0fefae709496a579b63607c
webDotG <webdotg.inbox@gmail.com>
2023-03-30 19:11:30 +0300

    fix new programm

--- js-test/messendger-programma.js
+++ js-test/messendger-programma.js
@@ -52,23 +52,23 @@ newMessageForm.addEventListener('submit', function(evt) {
 });
 
 // ==============================================
-var chat = document.querySelector(’.chat-content’);
+var chat = document.querySelector('.chat-content');
 var messages = chat.children;
-var newMessageForm = document.querySelector(’.chat-form’);
-var newMessageInput = document.querySelector(’.chat-form-input’);
-var messageTemplate = document.getElementById(‘message-template’).content;
-var newMessageTemplate = messageTemplate.querySelector(’.chat-message’);
+var newMessageForm = document.querySelector('.chat-form');
+var newMessageInput = document.querySelector('.chat-form-input');
+var messageTemplate = document.getElementById('message-template').content;
+var newMessageTemplate = messageTemplate.querySelector('.chat-message');
 
-newMessageForm.addEventListener(‘submit’, function (evt) {
+newMessageForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
     var messageText = newMessageInput.value;
     var message = newMessageTemplate.cloneNode(true);
-    var messageBox = message.querySelector(‘p’);
+    var messageBox = message.querySelector('p');
     messageBox.textContent = messageText;
     chat.appendChild(message);
-    var deleteButton = message.querySelector(’.chat-message-button’);
-    deleteButton.addEventListener(‘click’, function () {
+    var deleteButton = message.querySelector('.chat-message-button');
+    deleteButton.addEventListener('click', function () {
         message.remove();
     });
-    newMessageInput.value = ‘’;
+    newMessageInput.value = '';
 })
